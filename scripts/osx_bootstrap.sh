3b4e7416423bcf36b8de75e467726ef3f85f703a
Marcus Gomes <mvp.gomes1989@gmail.com>
2020-02-24 12:11:57 +0000

    Add script to bootstrap osx

---
+++ scripts/osx_bootstrap.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+# 
+# Bootstrap script for setting up a new OSX machine
+# 
+# This should be idempotent so it can be run multiple times.
+#
+# Notes:
+#
+# - If installing full Xcode, it's better to install that first from the app
+#   store before running the bootstrap script. Otherwise, Homebrew can't access
+#   the Xcode libraries as the agreement hasn't been accepted yet.
+#
+# Reading:
+#
+# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
+# - https://news.ycombinator.com/item?id=8402079
+# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
+
+echo "Starting bootstrapping"
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew tap homebrew/dupes
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install gnu-grep --with-default-names
+
+
+# Install Emacs Plus
+echo "Installing Emacs Plus..."
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+ln -s /usr/local/Cellar/emacs-mac/*/Emacs.app/ /Applications
+
+# Install Spacemacs
+echo "Installing Spacemacs..."
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+cp ../.spacemacs ~/
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+PACKAGES=(
+    elixir 
+    git
+    ispell
+    jq
+    tree
+    vim
+    zsh
+    wget
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Installing cask..."
+brew install caskroom/cask/brew-cask
+
+CASKS=(
+    google-chrome
+    iterm2
+    skype
+    slack
+    spectacle
+)
+
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+echo "Installing Ruby gems"
+RUBY_GEMS=(
+    bundler
+)
+sudo gem install ${RUBY_GEMS[@]}
+
+
+echo "Installing Oh My Zsh..."
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ../.zshrc ~/
+
+
+echo "Configuring OSX..."
+
+# Set fast key repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Show filename extensions by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Creating folder structure..."
+[[ ! -d Wiki ]] && mkdir Development
+
+echo "Bootstrapping complete"
